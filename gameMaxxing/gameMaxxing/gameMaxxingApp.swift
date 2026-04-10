56d58dff51de2d031a4116a2eba314725299f7a9
Lalitya Muktineni <lalityamuktineni@lalityas-mbp.myfiosgateway.com>
2026-04-09 20:46:51 -0400

    planning mode changes

---
+++ gameMaxxing/gameMaxxing/gameMaxxingApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameMaxxingApp.swift
+//  gameMaxxing
+//
+//  Created by Lalitya Muktineni on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct gameMaxxingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
